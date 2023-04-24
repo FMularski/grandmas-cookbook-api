ba3747f7fb6d9bd73eb2b1ca42c4bec32ebea086
Filip <filipelblag@gmail.com>
2023-04-24 15:12:32 +0200

    alter User model (type field)

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,46 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("password",)}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = (
+        "email",
+        "type",
+        "is_staff",
+    )
+    list_filter = ("is_staff", "type", "is_superuser", "is_active", "groups")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(User, CustomUserAdmin)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,53 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    pass
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    TYPE_GRANDMA = "G"
+    TYPE_MEMBER = "M"
+
+    TYPE_CHOICES = (
+        (TYPE_GRANDMA, _("Grandma")),
+        (TYPE_MEMBER, _("Member")),
+    )
+
+    username = None
+    first_name = None
+    last_name = None
+    email = models.EmailField(unique=True)
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default=TYPE_MEMBER)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
