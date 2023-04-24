3873c09840a3c21b034faa797172421551a64265
Filip <filipelblag@gmail.com>
2023-04-24 20:07:20 +0200

    create_cookbook signal

--- cookbooks/apps.py
+++ cookbooks/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CookbooksConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cookbooks"
+
+    def ready(self):
+        import cookbooks.signals  # noqa
