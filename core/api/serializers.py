91f8fc9349b6bb4cd31e7ce31408ec0c379e24d8
Filip <filipelblag@gmail.com>
2023-04-25 12:49:27 +0200

    full user type in /toke/ response

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -5,7 +5,8 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        data["type"] = self.user.type
+        type = list(filter(lambda t: t[0] == self.user.type, self.user.TYPE_CHOICES))[0][1]
+        data["type"] = type
 
         return data
 
