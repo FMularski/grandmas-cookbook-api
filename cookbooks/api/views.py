20110fb7f75daaf2555bac3bae61fc664bef6408
Filip <filipelblag@gmail.com>
2023-04-25 11:40:31 +0200

    order query parameter for /recipes/ endpoint

--- cookbooks/api/views.py
+++ cookbooks/api/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
 
 from cookbooks.models import Recipe
@@ -7,4 +9,17 @@ from .serializers import RecipeSerializer
 
 class RecipeListAPIView(generics.ListAPIView):
     serializer_class = RecipeSerializer
-    queryset = Recipe.objects.all()
+
+    def get_queryset(self):
+        order = self.request.GET.get("order")
+        return Recipe.objects.all().order_by(f"-{order}") if order else Recipe.objects.all()
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "order", openapi.IN_QUERY, type=openapi.TYPE_STRING, enum=["rating", "created_at"]
+            )
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
